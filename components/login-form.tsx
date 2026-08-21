9d79f53b08b70ac6f5a98dfe7873f710cd470458
Michal Němec <michal.nemec.86@gmail.com>
2026-08-21 09:48:40 +0200

    Prepare invite-only authentication

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -90,14 +90,8 @@ export function LoginForm({
                 {isLoading ? "Logging in..." : "Login"}
               </Button>
             </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link
-                href="/auth/sign-up"
-                className="underline underline-offset-4"
-              >
-                Sign up
-              </Link>
+            <div className="mt-4 text-center text-sm text-muted-foreground">
+              TasteApp je pouze pro pozvané uživatele.
             </div>
           </form>
         </CardContent>
