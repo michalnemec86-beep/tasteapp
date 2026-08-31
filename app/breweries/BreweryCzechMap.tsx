e07c3b779a4304997ee98a5e25e413a45f7e8b22
Michal Němec <michal.nemec.86@gmail.com>
2026-08-31 14:28:51 +0200

    Replace CARTO brewery map tiles

--- app/breweries/BreweryCzechMap.tsx
+++ app/breweries/BreweryCzechMap.tsx
@@ -178,8 +178,10 @@ export default function BreweryCzechMap({
           }}
         >
           <TileLayer
-            attribution='&copy; OpenStreetMap contributors &copy; CARTO'
-            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            className="brewery-map-tiles"
+            maxZoom={19}
           />
 
           <CzechMapViewport />
