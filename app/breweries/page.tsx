76e706676b40131141d61ba53589d58179397bee
michalnemec86-beep <michal.nemec.86@gmail.com>
2026-09-18 14:06:19 +0200

    Keep brewery map drilldown on brewery catalog

--- app/breweries/page.tsx
+++ app/breweries/page.tsx
@@ -790,6 +790,7 @@ export default async function BreweriesPage({
                   eyebrow="Pivovarský svět"
                   title="Mapa evidovaných pivovarů"
                   countLabel="států s pivovary"
+                  countryLinkMode="breweries"
                   focusEurope
                 />
               </div>
